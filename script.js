820d969e5baf2a2aa5709854f7216f5b12c1bc9a
Nikolay-Developer <example@gmail.com>
2025-01-23 12:19:39 +0200

    script.js mod

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ console.log(isChecked || !isClose);
 
 // Приложение по учету просмотренных фильмов
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/*const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -176,6 +176,77 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;   
 
 console.log(personalMovieDB);
+*/
+
+/* if (1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error');
+*/
+
+const num = '50';
+
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '50':
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
